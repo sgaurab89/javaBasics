6f7f363b1275429ffec05512b080a708ed33b877
sgaurab89 <gaurab.sharma@lntinfotech.com>
2023-05-11 23:27:14 +0530

    First commit

---
+++ JavaSessionsMay2023/src/com/qa/generalPrograms/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.qa.generalPrograms;
+
+import java.util.*;
+public class HelloWorld {
+
+public static void main(String[] args) {
+	
+	System.out.println("Hello World");
+	
+	String s = "India is my country";
+	System.out.println(s);
+	
+	int l= s.length();
+	
+	System.out.println("Length of the string is: "+l);
+	for(int i=0;i>=l-1;i++) {
+		System.out.println(s.charAt(i));
+	}
+	
+	
+	
+	
+	
+}	
+	
+	
+}
